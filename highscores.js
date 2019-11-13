35423b39e5ae1ba22933a9d7ebc634b8aa0af208
daniemeas <danie.meas@gmail.com>
2019-11-13 14:14:49 -0600

    display high scores on page

--- highscores.js
+++ highscores.js
@@ -1,8 +1,9 @@
 const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-console.log(highScores.map(score => {
-  return `<li class="high-score">(${score.name} -${score.score}</li>`;
+highScoresList.innerHTML =highScores.
+map(score => {
+  return `<li class="high-score">${score.name} - ${score.score}</li>`;
 })
-);
+.join('');
 
